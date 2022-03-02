de9caac7951395986bf508f7b23ef2db30913259
Herostere <bossartlaurent@gmail.com>
2022-03-02 21:50:07 +0100

    update

--- fetch_actions_names/fetch_actions_names_config.py
+++ fetch_actions_names/fetch_actions_names_config.py
@@ -1,11 +1,11 @@
 """
 Configuration file for "fetch_actions_names.py".
 """
-run = False
+run = True
 
 get_page_index = {
     "start_index": 50,
-    "max_threads": 50,
+    "max_threads": 5,
 }
 
 name_verification = True
