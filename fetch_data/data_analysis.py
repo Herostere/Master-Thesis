9d8d16ca9d73a61a5851e1c227ccf94b5967cb19
Herostere <bossartlaurent@gmail.com>
2022-05-18 15:14:20 +0200

    retrieving active developers

--- fetch_data/data_analysis.py
+++ fetch_data/data_analysis.py
@@ -7,6 +7,7 @@ from fetch_data import request_to_api, get_request, beautiful_html, get_remainin
 from packaging import version as packaging_version
 
 import data_analysis_config as config
+import heapq
 import json
 import math
 import matplotlib.pyplot as plt
@@ -460,6 +461,31 @@ def check_issues() -> None:
 
     print(open_issues, closed_issues)
     print(open_up_to_date, open_not_up_to_date)
+
+
+def check_contributors() -> None:
+    """
+    Retrieve the 20 most active contributors.
+    """
+    contributors = {}
+    for action in loaded_data.keys():
+        action_contributors = loaded_data[action]["contributors"]
+        for contributor in action_contributors:
+            if contributor not in contributors:
+                contributors[contributor] = 1
+            else:
+                contributors[contributor] += 1
+
+    most_active_with_bots = heapq.nlargest(20, contributors.items(), key=lambda i: i[1])
+
+    contributors_to_delete = [contributor for contributor in contributors if "bot" in contributor]
+    for contributor in contributors_to_delete:
+        del contributors[contributor]
+
+    most_active_without_bots = heapq.nlargest(20, contributors.items(), key=lambda i: i[1])
+
+    print(most_active_with_bots)
+    print(most_active_without_bots)
 
 
 if __name__ == "__main__":
