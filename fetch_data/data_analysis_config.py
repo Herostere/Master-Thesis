8c02efea4c39f3cb938a7dfcda5e6913037f820f
Herostere <bossartlaurent@gmail.com>
2022-07-19 09:46:18 +0200

    RQ4 config

--- fetch_data/data_analysis_config.py
+++ fetch_data/data_analysis_config.py
@@ -11,6 +11,7 @@ market_growing_over_time = False  # DONE
 actions_diversity = False  # DONE
 most_commonly_proposed = False  # DONE
 actions_technical_lag = False  # DONE
+rq4 = True
 rq5 = False
 rq7 = False
 
